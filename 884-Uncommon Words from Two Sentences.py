a22ce8ffb4f97cf9094f84cd382bfe56e95b8d38
marcos3777 <marcos_vinicius_11@hotmail.com>
2024-10-02 16:32:22 -0300

    Add solution for finding uncommon words from two sentences

---
+++ 884-Uncommon Words from Two Sentences.py
@@ -0,0 +1,16 @@
+from typing import Counter, List
+
+
+class Solution:
+    def uncommonFromSentences(self, s1: str, s2: str) -> List[str]:
+        words1 = s1.split() 
+        words2 = s2.split()
+        wordCount = Counter(words1 + words2)
+        uncommonWords = []
+
+        for word in wordCount:
+            if wordCount[word] == 1:
+                uncommonWords.append(word)
+
+        return uncommonWords
+    
